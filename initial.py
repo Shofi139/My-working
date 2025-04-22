0e6f910f475a7bb0ad85690480cff195795a8cf1
Md Shofiulla <mdshofiulla139@gmail.com>
2025-04-22 23:20:38 +0600

    Initial commit

---
+++ initial.py
@@ -0,0 +1,12 @@
+hello = "Hello World"
+print(hello)
+S = "I am Shofiulla"
+print(S)
+Best = "Best of luck"
+print(Best)
+WWW = """Hello World.
+I am Shofiulla.
+ Best of luck. """
+print(WWW)
+SSS = "THIS IS MY FIRST PROGRAM"
+print(SSS)
